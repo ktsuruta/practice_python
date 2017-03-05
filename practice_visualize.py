61b4b71fa87cb85006344a1fd6e1950fd196a083
鶴田健 <tsurutaken@tsurutaken-no-MacBook-Pro.local>
2017-03-05 17:48:47 +0900

    commit

---
+++ practice_visualize.py
@@ -0,0 +1,31 @@
+
+from matplotlib import pyplot as plt
+
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
+plt.title("Nominal GDP")
+plt.ylabel("billions of $")
+#plt.show()
+
+movies = [
+    "Annie Hall",
+    "Ben-Hur",
+    "Casablanca",
+    "Gandhi",
+    "West Side Story"
+]
+num_oscars = [5,11,3,8,10]
+
+xs = [i + 0.1 for i, _ in enumerate(movies)]
+
+print(xs)
+
+plt.bar(xs, num_oscars)
+
+plt.ylabel("# of Academy Award")
+plt.title("My Favorite Movie")
+
+plt.xticks([i + 0.5 for i, _ in enumerate(movies)], movies)
+
+plt.show()
